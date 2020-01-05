cfc24695028431c9e1a6c7c133ac1eb96c2dadb5
Sunny Bharadwaj <sunny@chakradesign.co>
2020-01-06 01:13:23 +0530

    styleguide - type and colors done. Buttons in progress

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,35 +1,18 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{--Add Meta Tags--}}
-    <link rel="stylesheet" href="https://use.typekit.net/hse8jpo.css">
-    {{--<meta name="description" content="Be Smart enables enterprises achieve their Energy Management, Automation and Security goals through Artificial Intelligence and Machine Learning. It helps IoTize your homes and businesses such as retail, F&B and other enterprises or chains by delivering real time data about your energy consumption, occupancy levels and security threats. Be Smart ensures a return on investment in under 6 months*.">--}}
-    {{--<meta name="keywords" content="Home Automation, Enterprise Automation, Office Automation, Wireless Automation, Energy wastage reduction, Food wastage reduction, Unplanned downtime reduction, Energy cost reduction, Smart Automation, Secure Automation, Security Monitoring, Theft Prevention, Smart security, Energy Management, Energy Saving, Load Monitoring, Load Optimization, Energy Optimization, Energy Monitoring, Real Time Usage Analysis, Energy Budget Control, Malfunction Detection, Malfunction alert, Preventive maintenance alert, Increased asset reliability, Smart Home Automation, Best Home Automation, Home Automation and Security System, Smart Automation and Security System, Best Automation System, Smart Enterprise Automation System, Smart Office Automation System, Wireless Automation and Control System, Reduce cost of energy consumption, Security Monitoring Solution, Theft Prevention Solution, Smart security for home, Smart security for enterprises, Smart security for offices, Energy Management Solution, Real Time Energy Saving, Real Time Load Monitoring, Real Time Load Optimization, Real Time Energy Optimization, Real Time Energy Monitoring, Real Time Usage Analysis, Malfunction Detection Solution, Malfunction alert system, Preventive maintenance alert solution, Return on Investment">--}}
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
-    <title>The Dot Express</title>
-</head>
-<body>
+@extends('layout')
+@section('content')
+    @include('components/navigation')
 
-<h1>The Dot Express</h1>
-@include('components/navigation')
-@include('components/header')
+    @include('pages/header')
+    @include('pages/explore')
+    @include('pages/voice')
+    @include('pages/experience')
 
-@include('pages/explore')
-@include('pages/voice')
-@include('pages/experience')
+    @include('components/announcements')
 
-@include('components/announcements')
+    @include('pages/partners')
+    @include('pages/about')
 
-@include('pages/partners')
-@include('pages/about')
-
-@include('components/contact_form')
-@include('components/pledge_form')
-@include('components/footer')
-<script src="{{mix('/js/app.js')}}"></script>
-</body>
-</html>
+    @include('components/contact_form')
+    @include('components/pledge_form')
+    @include('components/footer')
+@endsection
---
+++ resources/views/styleguide.blade.php
@@ -0,0 +1,123 @@
+@extends ('layout')
+@section('content')
+    <div id="styleguide">
+        <div class="container">
+            <div class="max-w-5xl mx-auto">
+                <div class="colors">
+                    <h2 class="h5">Colors</h2>
+                    <div class="primary">
+                        <h2 class="h5 small title">Primary</h2>
+                        <div class="color-block bg-primary"></div>
+                    </div>
+
+                    <div class="primary">
+                        <h2 class="h5 small title">Secondary</h2>
+                        <div class="color-block bg-secondary">
+
+                        </div>
+                    </div>
+                    <div class="functional">
+                        <h2 class="h5 small title">Functional</h2>
+                        <div class="color-block bg-black"></div>
+                        <div class="color-block bg-d-gray"></div>
+                        <div class="color-block bg-l-gray"></div>
+                        <div class="color-block bg-ll-gray"></div>
+                        <div class="color-block bg-highlight"></div>
+                    </div>
+
+                    <div class="accents">
+                        <h2 class="h5 small title">Accents</h2>
+                        <div class="color-block bg-a1"></div>
+                        <div class="color-block bg-a2"></div>
+                        <div class="color-block bg-a3"></div>
+                    </div>
+                    <div class="backgrounds">
+                        <h2 class="h5 small title">backgrounds</h2>
+                        <div class="color-block bg-a1-l"></div>
+                        <div class="color-block bg-a2-l"></div>
+                        <div class="color-block bg-a3-l"></div>
+                    </div>
+                </div>
+                <div class="buttons">
+                    <div class="block">
+                        <h2 class="h5 title">Standard Buttons</h2>
+                        <a href="" class="primary-btn">Pledge Support</a>
+                        <a href="" class="secondary-btn">Buy Book</a>
+                        <h2 class="h5 title">Text Buttons</h2>
+                        <a class="text-btn">Write to us</a>
+                        <a class="text-btn sm">Continue reading</a>
+                        <a class="text-btn lg">Submit work</a>
+                    </div>
+                </div>
+                <div class="typography max-w-3xl mx-auto mt-24">
+                    <h2 class="h5 text-center">Typography</h2>
+                    <div class="block">
+                        <p class="title">h1</p>
+                        <h1 class="h1">You know when grown-ups tell you everything.</h1>
+                    </div>
+                    <div class="block">
+                        <p class="title">h2</p>
+                        <h2 class="h2">You'd think they’re probably lying to make you feel better.</h2>
+                    </div>
+                    <div class="block">
+                        <p class="title">h3</p>
+                        <h3 class="h3">You know when grown-ups tell you ‘everything’s going to be fine’ and you think
+                            they’re probably lying to make you feel better?</h3>
+                    </div>
+                    <div class="block">
+                        <p class="title">h4</p>
+                        <h4 class="h4">You know when grown-ups tell you ‘everything’s going to be fine’ and you think
+                            they’re probably lying to make you feel better?</h4>
+                    </div>
+                    <div class="block">
+                        <p class="title">h5</p>
+                        <h1 class="h5">Announcements</h1>
+                    </div>
+                    <div class="block">
+                        <p class="title">hl</p>
+                        <p class="hl">You know when grown-ups tell you ‘everything’s going to be fine’ and you think
+                            they’re probably lying to make you feel better? You know when grown-ups tell you
+                            ‘everything’s going to be fine’ and you think they’re probably lying to make you feel
+                            better?</p>
+                    </div>
+                    <div class="block">
+                        <p class="title">p</p>
+                        <p>You know when grown-ups tell you ‘everything’s going to be fine’ and you think they’re
+                            probably lying to make you feel better? You know when grown-ups tell you ‘everything’s going
+                            to be fine’ and you think they’re probably lying to make you feel better?</p>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+    {{--buttons--}}
+    {{--Standard Buttons--}}
+    {{--primary--}}
+    {{--secondary--}}
+    {{--text buttons--}}
+    {{--Types--}}
+    {{--basic--}}
+    {{--loud--}}
+    {{--Sizes--}}
+    {{--regular--}}
+    {{--small--}}
+    {{--large--}}
+
+    {{--typography--}}
+    {{--h1--}}
+    {{--h2--}}
+    {{--h3--}}
+    {{--h4--}}
+    {{--h5--}}
+    {{--p.hl--}}
+    {{--p--}}
+
+
+@endsection
