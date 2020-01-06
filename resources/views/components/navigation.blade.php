74a182c8bcaf361b8de07ac756b601f8c287bf65
Sunny Bharadwaj <sunny@chakradesign.co>
2020-01-07 00:59:56 +0530

    Style run 1 - nav and header done. Onto content sections

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,22 +1,40 @@
-<nav>
-    <div class="logo hidden">
-        <img src="" alt="">
-    </div>
+<nav id="main-nav">
+
     <div class="nav-wrapper">
-        <div class="left-block">
-            Explore
-            Voice
-            Experience
-        </div>
-
-        <div class="right-block">
-            <div class="primary-button">
-                Pledge Support
+        <div class="flex items-center justify-between">
+            <div class="left-block">
+                <div class="logo hidden">
+                    @include ('snippets/logo')
+                </div>
+                <div class="links">
+                    <a href="#explore" class="nav-link hl">
+                        <span class="text">Explore</span>
+                        <span class="blob">
+                        @include('snippets/blob1')
+                    </span>
+                    </a>
+                    <a href="#voice" class="nav-link hl">
+                        <span class="text">Voice</span>
+                        <span class="blob">
+                        @include('snippets/blob2')
+                    </span>
+                    </a>
+                    <a href="#experience" class="nav-link hl">
+                        <span class="text">Experience</span>
+                        <span class="blob">
+                        @include('snippets/blob3')
+                    </span>
+                    </a>
+                </div>
             </div>
-            <div class="primary-button">
-                Buy Book
+            <div class="right-block">
+                <div class="std-btn primary sm">
+                    Pledge Support
+                </div>
+                <div class="std-btn primary sm">
+                    Buy Book
+                </div>
             </div>
-
         </div>
     </div>
 </nav>
--- resources/views/pages/header.blade.php
+++ resources/views/pages/header.blade.php
@@ -1,24 +1,32 @@
-<section>
-    <div class="section-wrapper">
-        <div class="header-bg">
-            <div class="content-wrapper">
-                <div class="header-text">
-                    <div class="logo">
-                        <img src="" alt="">
+<header>
+    <section>
+        <div class="section-wrapper">
+            <div class="header-bg">
+                <div class="content-wrapper py-24">
+                    <div class="header-text">
+                        <div class="logo mb-2">
+                            <div class="wrapper">
+                                <div class="max-w-lg mx-auto">
+                                    @include ('snippets/logo')
+                                </div>
+                            </div>
+                        </div>
+                        <div class="max-w-2xl mx-auto text-center">
+                            <p class="hl">A digital platform for young generation to Voice their thoughts, Explore new Ideas and build upon shared Experiences.</p>
+                        </div>
                     </div>
-                    <p>A digital platform for young generation to Voice their thoughts, Explore new Ideas and build upon shared Experiences.</p>
                 </div>
-            </div>
 
-            <div class="announcements-notification">
-                <div class="content-wrapper">
-                    <div class="h4">Announcement text</div>
-                    <div class="links">
-                        <div class="primary-link">Learn More</div>
-                        <div class="Secondary-link">View All Announcements</div>
+                <div class="announcements-notification">
+                    <div class="content-wrapper">
+                        <div class="h4">Announcement text</div>
+                        <div class="links">
+                            <div class="primary-link">Learn More</div>
+                            <div class="Secondary-link">View All Announcements</div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+</header>
